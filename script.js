64e65f8ef5997f86f2156905faa7e312fa6ae0c8
Manuel Felipe Gonzalez <manuelg1234456@gmail.com>
2026-05-23 17:56:46 -0500

    Se crea todas las tablas para mto preventivo, se crea logica par añadir los insumos a demanda con un click, se esta construyendo la logica para recoger los datos que selecciona el tecnico aun no esta finalizado

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const tipoServicio = document.getElementById("tipoServicio");
 const SeccionEntrega= document.getElementById("entrega");
 const SeccionPreventivo = document.getElementById("preventivo");
+
+//Seccion insumos mto prev
+const tablaInsumosPreventivo =  document.getElementById("tablaInsumosPreventivo");
+const botonAñadir = document.getElementById("agregarInsumo");
+const filaInsumo= document.getElementsByClassName("fila-insumo");
 //Fotos
 const fotoMontacarga = document.getElementById("fotoMontacarga");
 const contenedorFotosMontacarga = document.getElementById("contenedorFotosMontacarga");
@@ -23,6 +28,7 @@ const lienzoVacio = firmaTecnico.toDataURL();
 let fotosMontacargaBase64=[];//Array para guarar las fotos que subamos del montacarga en base 64
 let fotosBateriaBase64=[];//Array para guarar las fotos que subamos de bateria en base 64
 let fotosCargadorBase64=[];//Array para guarar las fotos que subamos del cargador en base 64
+
 //Funcion para ocultar o mostrar secciones dependiendo del tipo de servicio seleccionado
 tipoServicio.addEventListener("change",function hola(){
     SeccionEntrega.style.display = "none";
@@ -81,6 +87,43 @@ fotoCargador.addEventListener("change",function mostrarFoto(){
 fotoBateria.addEventListener("change", function mostrarFoto(){
     procesarFotos(contenedorFotosBateria,fotoBateria,fotosBateriaBase64);
 })
+
+//Listener para boton de agregar Insumos
+botonAñadir.addEventListener("click",function(){
+
+    tablaInsumosPreventivo.insertAdjacentHTML("beforeend",`
+        <tr class="fila-insumo">
+        <td>
+        <select name="insumo" class="insumo" >
+                            <option value="0" disabled selected>Selecciona un insumo</option>
+                            <option value="1">Desengrasante</option>
+                            <option value="2">Agua de bateria</option>
+                            <option value="3">Silicona</option>
+                            <option value="4">Bolsa de trapos</option>
+                        </select>
+        </td>
+        <td>
+        <input type="number" class="cantidad" value="2" min="0" max="20">
+        </td>
+        <td>
+        <select name="medida" class="Medidainsumo" >
+                            <option value="0" disabled selected>Selecciona una medida</option>
+                            <option value="litros" name="Litros">Litros</option>
+                            <option value="bolsa" name="bolsa">Bolsa</option>
+                        </select>
+        </td>
+
+        </tr>`
+    )
+    for (let fila of filaInsumo) {
+
+    console.log(fila.querySelector(".insumo").value)
+    console.log(fila.querySelector(".cantidad").value)
+    console.log(fila.querySelector(".Medidainsumo").value)
+}
+
+})
+
 
 //Espacio dibujo de firma 
 //Variables
